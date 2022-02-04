735a49f2a095f5468cb48310228a03e32fd48c00
Isaac <zaragozamendozaisaac@gmail.com>
2022-02-04 19:33:11 +0100

    fix db provider

--- src/clients/discord.js
+++ src/clients/discord.js
@@ -1,13 +1,8 @@
 const { Client, Collection } = require("discord.js");
-const Levels = require("discord-xp");
-
-Levels.setURL(process.env.DATABASE_URI);
-const levels = Levels;
 
 class Kohaku extends Client {
   commands = new Collection();
   collectors = new Collection();
-  levels = levels;
 }
 
 module.exports = Kohaku;
