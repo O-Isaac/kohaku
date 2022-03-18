d34e617b62e29b717988455173e8b9409c7cafed
Isaac <48893799+IsaacZM@users.noreply.github.com>
2022-03-18 17:29:58 +0100

    Anti NSFW

---
+++ src/events/messageCreate.js
@@ -0,0 +1,13 @@
+const { Message } = require("discord.js");
+const NSFWFilter = require("../services/nsfw.filter");
+
+module.exports = {
+  name: "messageCreate",
+  /**
+   * Main messageCreate event
+   * @param {Message} message
+   */
+  async execute(message) {
+    NSFWFilter(message);
+  },
+};
