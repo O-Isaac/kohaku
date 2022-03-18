14bb6584b3d84f405e9e545905de10bf62bef6e6
Isaac <48893799+IsaacZM@users.noreply.github.com>
2022-03-18 18:02:27 +0100

    new anti nsfw system

--- src/services/nsfw.filter.js
+++ src/services/nsfw.filter.js
@@ -1,13 +1,29 @@
 const { Message, MessageEmbed } = require("discord.js");
 const fetch = require("node-fetch");
+const { AntiNsfwClient } = require("discord-antinsfw");
+
+const nsfw = new AntiNsfwClient();
 
 /**
  * Filtra el contenido de un mensaje para que no sea para adultos!
  * @param {Message} message
  */
 module.exports = async function NSFWFilter(message) {
-  const image = message.attachments.size > 0 ? message.attachments : null;
-
-  if (image) {
-  }
+  nsfw.init(message);
 };
+
+nsfw.on("nsfw", (message, data) => {
+  const embed = new MessageEmbed()
+    .setTitle("NSFW Detectado!")
+    .setDescription(
+      "Este mensaje contiene contenido NSFW, por favor, no lo envíes a este canal."
+    )
+    .addField("Detections", data.detections.join(", "));
+
+  const isNFSW = data.confidence.replace("%", "") > 20;
+
+  if (data.isNSFW && isNFSW) {
+    message.delete();
+    message.channel.send({ embeds: [embed] });
+  }
+});
