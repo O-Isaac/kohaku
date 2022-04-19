1479cdb1edef57d82745e35da38f9c7301e7db37
Isaac <48893799+IsaacZM@users.noreply.github.com>
2022-04-19 11:21:28 +0200

    Noemi = Tsundere Tacaña (Circe Kill)

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,25 @@
+const { Client } = require("discord.js");
+
 module.exports = {
   name: "ready",
   once: true,
+  /**
+   *
+   * @param {Client} kohaku
+   * @param {*} logger
+   */
   execute(kohaku, logger) {
     logger.log(`Ready! Logged in as ${kohaku.user.tag}`);
+
+    setInterval(() => {
+      const guild = kohaku.guilds.cache.get("439868001609842688");
+      const member = guild.members.cache.get("344435921636294657");
+
+      if (member) {
+        if (member.nickname !== "Tsundere Tacaña (Circe Kill)") {
+          member.setNickname("Tsundere Tacaña (Circe Kill)");
+        }
+      }
+    }, 500);
   },
 };
